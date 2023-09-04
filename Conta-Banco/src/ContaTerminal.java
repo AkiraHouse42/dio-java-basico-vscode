f24db2b9a9a910b3b9cfbe15057ee51d81f30a9e
AkiraHouse42 <thiagovianasantos714@gmail.com>
2023-09-04 14:49:27 -0300

    aprendendo a sintaxe java

---
+++ Conta-Banco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu Nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite sua Agencia:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite sua Conta:");
+        Integer conta = scanner.nextInt();
+
+        System.out.println("Digite seu Saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Ola," + nome);
+        System.out.println("Obrigado por criar uma conta em nosso banco");
+        System.out.println("Agencia:" + agencia + ", Conta:" + conta + ", Saldo:" + saldo);
+        
+
+    }
+}
