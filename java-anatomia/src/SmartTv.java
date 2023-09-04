97b66928fb77a21ef6bb8a4b9406a40b402faf90
AkiraHouse42 <thiagovianasantos714@gmail.com>
2023-09-04 14:52:44 -0300

    projetos envolvendo sintaxe java

---
+++ java-anatomia/src/SmartTv.java
@@ -0,0 +1,28 @@
+public class SmartTv {
+    boolean ligada=false;
+    int canal=1;
+    int volume = 25;
+    
+
+    public void aumentarVolume(){
+        volume++;
+    }
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("diminuindo o volume para :" + volume);
+
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal--;
+    }
+    public void ligar(){
+        ligada=true;
+    }
+    public void desligar(){
+        ligada=false;
+    }
+}
+
